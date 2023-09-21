ed3301a4b1428677cc2ee361b70cae3f05db423d
Cherry-Fish <105974366+Cherry-Fish@users.noreply.github.com>
2023-09-21 10:42:01 +0900

    Add files via upload

---
+++ chap2/prob1.py
@@ -0,0 +1,6 @@
+print('Game Over\n')
+
+while True:
+    n = input('Press the enter key to exit.')
+    if n == '':
+        break;
---
+++ chap2/prob2.py
@@ -0,0 +1,13 @@
+car = int(input('Lamborghini Tune-Ups: '))
+rent =int( input('Manhattan Apartment: '))
+jet = int(input('Private Jet Rental: '))
+gifts = int(input('Gifts: '))
+food = int(input('Dining Out: '))
+staff = int(input('Staff (butlers, chef, driver, assistant): '))
+guru = int(input('Persoanl Guru and Coach: '))
+games = int(input('Computer Game: '))
+
+total=car+rent+jet+gifts+food+staff+guru+games
+print(f'Grand Total : {total}')
+
+input("\n\nPress the enter key to exit.")
