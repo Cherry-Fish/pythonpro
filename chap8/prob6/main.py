85488b300c07ab8bf3f234c7f0fc5f97bf7b53f5
Cherry-Fish <acvse@cu.ac.kr>
2023-11-21 23:51:27 +0900

    d

--- chap8/prob6/main.py
+++ chap8/prob6/main.py
@@ -1,28 +1,32 @@
 import random
 
-# Load questions from files
-questions = []
-for file_path in ["Q1.txt", "Q2.txt", "Q3.txt", "Q4.txt"]:
-    with open(file_path, 'r', encoding='utf-8') as file:
-        lines = file.read().split('\n')
-        question = {
-            'question': lines[0][3:].strip(),
-            'answers': [line[3:].strip() for line in lines[1:4]],
-            'correct_answer': lines[4][3:].strip(),
-        }
-        questions.append(question)
+def load_questions(filet):
+    questions = []
+    for filet in filet:
+        with open(filet, 'r') as file:
+            lines = file.read().split('\n')
+            question = {
+                'question': lines[0][3:].strip(),
+                'answers': [line[3:].strip() for line in lines[1:4]],
+                'correct_answer': lines[4][0].strip(), 
+            }
+            questions.append(question)
+    return questions
 
-# Shuffle questions
-random.shuffle(questions)
+def trivia_game(questions):
+    random.shuffle(questions)
+    for question in questions:
+        print("\n-------------")
+        print(question['question'])
+        for i, answer in enumerate(question['answers'], start=1):
+            print(f"{i}. {answer}")
+        user_answer = input("\nEnter your answer (1, 2, 3): ")
+        if user_answer == question['correct_answer']:
+            print("correct answer!")
+        else:
+            print(f"wrong answer! correct answer : {question['correct_answer']}.")
 
-# Play trivia game
-for question in questions:
-    print("\n-------------")
-    print(question['question'])
-    for i, answer in enumerate(question['answers'], start=1):
-        print(f"{i}. {answer}")
-    user_answer = input("\nEnter your answer (1, 2, or 3): ")
-    if question['answers'][int(user_answer) - 1] == question['correct_answer']:
-        print("Correct!")
-    else:
-        print(f"Wrong! The correct answer is: {question['correct_answer']}")
+if __name__ == "__main__":
+    question_files = ["Q1.txt", "Q2.txt", "Q3.txt", "Q4.txt"]
+    trivia_questions = load_questions(question_files)
+    trivia_game(trivia_questions)
